a94ad27fdc269911189a6fb626a9c684f5b424bf
biniu <dawid.binkowski93@gmail.com>
2019-07-20 02:26:35 +0200

    add dailiesDone list serializer

--- IronHabbitApi/task/serializers.py
+++ IronHabbitApi/task/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task, Project, Habbit, Dailies
+from .models import Task, Project, Habbit, Dailies, DailiesDoneList
 
 
 class ProjectSerializers(serializers.ModelSerializer):
@@ -20,4 +20,9 @@ class HabbitSerializers(TaskSerializers):
 class DailiesSerializers(TaskSerializers):
     class Meta:
         model = Dailies
+        fields = '__all__'
+
+class DailiesDoneListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = DailiesDoneList
         fields = '__all__'
